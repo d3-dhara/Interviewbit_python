50ea79e4232945adc433a6c753a325783b62c1e9
d3-dhara <anubhosale1995@gmail.com>
2018-08-16 21:55:35 +0530

    Create Arrays_NextPermutation.py

---
+++ Arrays_NextPermutation.py
@@ -0,0 +1,20 @@
+class Solution:
+    # @param A : list of integers
+    # @return the same list of integer after modification
+    def nextPermutation(self, A):
+        n=len(A)
+        i=j=n-1
+        while(i>0):
+            if A[i]>A[i-1]:
+                break
+            i-=1
+        i-=1
+        if i<0:
+            return sorted(A)
+        while(j>i):
+            if A[j]>A[i]:
+                break
+            j-=1
+        A[i],A[j] = A[j],A[i]
+        A[i+1:n] = reversed(A[i+1:n])
+        return A
